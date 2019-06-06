e3b3342b3622ddb89d306c648227b8964433f3a5
Pegasis <admin@pegasis.site>
2019-06-06 19:12:29 -0400

    tray window finished

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, Menu, Tray, BrowserWindow, ipcMain} = require('electron');
+const electron = require('electron');
+const Store = require('electron-store');
+const store = new Store();
 
-let win;
+let welcomeWindow=null;
+let trayWindow=null;
+let tray=null;
 const debug=true;
 
-function createWindow() {
+
+function createWelcomeWindow() {
     if (debug) {
-        win = new BrowserWindow({
+        welcomeWindow = new BrowserWindow({
             frame: false,
             show: false,
             width: 854,
@@ -16,7 +22,7 @@ function createWindow() {
             }
         });
     }else {
-        win = new BrowserWindow({
+        welcomeWindow = new BrowserWindow({
             frame: false,
             show: false,
             width: 300,
@@ -28,42 +34,97 @@ function createWindow() {
         });
     }
 
+    welcomeWindow.loadFile('app/welcome.html');
 
-    win.loadFile('app/index.html');
-
-    win.once('ready-to-show', () => {
+    welcomeWindow.once('ready-to-show', () => {
         if (debug){
-            win.webContents.openDevTools();
+            welcomeWindow.webContents.openDevTools();
         }
 
-        win.show()
+        welcomeWindow.show()
     });
 
-    win.on('closed', () => {
-        win = null
+    welcomeWindow.on('closed', () => {
+        welcomeWindow = null
     })
 }
 
-app.on('ready', createWindow);
+function createTrayWindow(){
+    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+    trayWindow = new BrowserWindow({
+        alwaysOnTop: true,
+        frame: false,
+        show: false,
+        width: 300,
+        height: 300,
+        x: width-300,
+        y: height-300,
+        resizable: false,
+        skipTaskbar: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
+    trayWindow.loadFile('app/tray.html');
+
+    trayWindow.once('ready-to-show', () => {
+        trayWindow.show()
+    });
+
+    trayWindow.on('closed', () => {
+        trayWindow = null
+    });
+}
+
+function createTrayIcon(){
+    tray = new Tray('app/image/ic_icon.png');
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Exit',click(){app.quit()} },
+    ]);
+    tray.setToolTip(store.get("device_name")+": ready");
+    tray.setContextMenu(contextMenu);
+    tray.on("click",()=>{
+        if (trayWindow === null) {
+            createTrayWindow()
+        }else {
+            trayWindow.show()
+        }
+    });
+}
+
+function createSettingsWindow(){
+
+}
+
+app.on('ready', () =>{
+    let deviceName=store.get("device_name");
+    if (deviceName===undefined){
+        createWelcomeWindow();
+    } else {
+        createTrayIcon();
+        if (debug){
+            createTrayWindow();
+        }
     }
+
 });
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
-});
+app.on('window-all-closed', () => { });
 
 ipcMain.on("window-operation", (event, arg) => {
-
-    if (arg==="close"){
+    if (arg.window==="welcome" && arg.operation==="close"){
+        welcomeWindow.close();
         app.quit();
-    } else {
-        win.minimize();
+    }else if (arg.window==="tray" && arg.operation==="minimize"){
+        trayWindow.hide();
+    }
+});
 
+ipcMain.on("new-page",(event,arg) =>{
+    if (arg.from === "welcome" && arg.to==="tray"){
+        welcomeWindow.close();
+        createTrayIcon();
+        createTrayWindow();
     }
 });
